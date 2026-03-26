6e2967a21f53b2e196629c49f769cc0b4025a8cd
UshaM0701 <ushausham16@gmail.com>
2026-03-26 23:22:05 +0530

    Add  mini project personal info card generator

---
+++ 01_Basics/Day-01-Variables/mini_project.py
@@ -0,0 +1,14 @@
+
+print("=== Personal Info Card Generator ===")
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+college = input("Enter your college: ")
+goal = input("Enter your goal: ")
+
+print("\n--- Your Info Card ---")
+print("Name:", name)
+print("Age:", age)
+print("College:", college)
+print("Goal:", goal)
+print("----------------------")
