d6844c0505195a4cb6c8153e2a7b16e45af92e4e
UshaM0701 <ushausham16@gmail.com>
2026-03-26 23:19:16 +0530

    Add basic variable usage and input handling in practice.py

---
+++ 01_Basics/Day-01-Variables/practice.py
@@ -0,0 +1,22 @@
+# Variables
+name = "Usha"
+age = 20
+height = 5.3
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+
+# Taking input
+user_name = input("Enter your name: ")
+user_age = int(input("Enter your age: "))
+
+print("Welcome", user_name)
+print("You are", user_age, "years old")
+
+# Simple calculation
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+sum = num1 + num2
+print("Sum is:", sum)
