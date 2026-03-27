270cc083e9684c84ddc5ab53457a16ffaf3948c3
UshaM0701 <ushausham16@gmail.com>
2026-03-27 22:54:22 +0530

    Add practice.py for data types and string operations

---
+++ 01_Basics/Day-02-Data-Types-Strings/practice.py
@@ -0,0 +1,41 @@
+# Day 2 - Data Types & Strings
+
+# Data types
+age = 20
+height = 5.6
+name = "Usha"
+is_student = True
+
+print(type(age))
+print(type(height))
+print(type(name))
+print(type(is_student))
+
+# String operations
+first_name = "Usha"
+last_name = "M"
+
+full_name = first_name + " " + last_name
+print("Full Name:", full_name)
+
+# Repetition
+print("Hello! " * 3)
+
+# String functions
+text = "  python is fun  "
+
+print(text.upper())
+print(text.lower())
+print(text.title())
+print(text.strip())
+
+# Length
+print("Length:", len(text))
+
+# Indexing & slicing
+word = "Python"
+
+print(word[0])
+print(word[-1])
+print(word[0:3])
+print(word[2:])
